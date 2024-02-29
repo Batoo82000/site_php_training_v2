58cdd09b4a368d4705424e117ff9361c5da111a4
unknown <batoo82000@gmail.com>
2024-02-29 10:32:36 +0100

    mise à jour du fichier nanasite2.sql

--- views/back/adminNews.view.php
+++ views/back/adminNews.view.php
@@ -3,6 +3,13 @@ ob_start();
 echo styleTitreNiveau1("Page d'aministration des news", COLOR_TITRE_ADMIN);
 ?>
 
+<div class="form-row">
+    <div class="form-group col-6">
+        <label for="titreActu">Titre de l'actualité : </label>
+        <input type="text" class="form-control" id="titreActu" name="titreActu" required/>
+    </div>
+</div>
+
 <div class="row">
     <div class="col text-center">
         <form method="POST" action="">
